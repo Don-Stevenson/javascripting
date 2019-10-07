07c1c4b41776051dc869fa13c6e901be8e7f5fa2
Don-Stevenson <dsteven1@gmail.com>
2019-10-07 01:14:50 +0000

    objects work

---
+++ objects.js
@@ -0,0 +1,13 @@
+// In that file, define a variable named pizza like this:
+
+    const pizza = {
+      toppings: ['cheese', 'sauce', 'pepperoni'],
+      crust: 'deep dish',
+      serves: 2
+    }
+
+console.log(pizza)
+
+ // Check to see if your program is correct by running this command:
+ //
+ //    javascripting verify objects.js
