c84c115e60911997b88a18b6120b71b8608989e3
Don-Stevenson <dsteven1@gmail.com>
2019-10-07 01:15:36 +0000

    object properties work

---
+++ object-properties.js
@@ -0,0 +1,10 @@
+// ## The challenge:
+//
+//  Create a file named object-properties.js.
+//
+//  In that file, define a variable named food like this:
+
+    const food = {
+      types: 'only pizza'
+    }
+console.log(food.types) 
